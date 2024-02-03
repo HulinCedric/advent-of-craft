59abad22341c091631988e938e8cb6e5e1eb44f0
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-03 23:12:58 +0100

    refactor: Alias of function
    Not touch production code contract

--- exercise/c#/Day01/Day01/Food.cs
+++ exercise/c#/Day01/Day01/Food.cs
@@ -1,16 +1,18 @@
 ﻿namespace Day01;
 
+using Clock = Func<DateOnly>;
+
 public record Food(
     DateOnly ExpirationDate,
     bool ApprovedForConsumption,
     Guid? InspectorId)
 {
-    public bool IsEdible(Func<DateOnly> now)
+    public bool IsEdible(Clock now)
         => IsFresh(now) &&
            CanBeConsumed() &&
            HaveBeenInspected();
 
-    private bool IsFresh(Func<DateOnly> now)
+    private bool IsFresh(Clock now)
         => ExpirationDate > now();
 
     private bool CanBeConsumed()
