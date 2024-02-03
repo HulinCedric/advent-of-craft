13a1b2b387f3701cddf79447735e51005ae160e4
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-03 22:57:17 +0100

    refactor: File scoped namespace

--- exercise/c#/Day01/Day01/Food.cs
+++ exercise/c#/Day01/Day01/Food.cs
@@ -1,22 +1,21 @@
-﻿namespace Day01
+﻿namespace Day01;
+
+public record Food(
+    DateOnly ExpirationDate,
+    bool ApprovedForConsumption,
+    Guid? InspectorId)
 {
-    public record Food(
-        DateOnly ExpirationDate,
-        bool ApprovedForConsumption,
-        Guid? InspectorId)
-    {
-        public bool IsEdible(Func<DateOnly> now)
-            => IsFresh(now) &&
-               CanBeConsumed() &&
-               HaveBeenInspected();
+    public bool IsEdible(Func<DateOnly> now)
+        => IsFresh(now) &&
+           CanBeConsumed() &&
+           HaveBeenInspected();
 
-        private bool IsFresh(Func<DateOnly> now)
-            => ExpirationDate > now();
+    private bool IsFresh(Func<DateOnly> now)
+        => ExpirationDate > now();
 
-        private bool CanBeConsumed()
-            => ApprovedForConsumption;
+    private bool CanBeConsumed()
+        => ApprovedForConsumption;
 
-        private bool HaveBeenInspected()
-            => InspectorId != null;
-    }
+    private bool HaveBeenInspected()
+        => InspectorId != null;
 }
