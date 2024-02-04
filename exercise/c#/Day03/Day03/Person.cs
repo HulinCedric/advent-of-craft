08d9f26fcff55f8f21081a7c73148ac245840920
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 18:18:53 +0100

    refactor: File scoped namespace

--- exercise/c#/Day03/Day03/Person.cs
+++ exercise/c#/Day03/Day03/Person.cs
@@ -1,16 +1,15 @@
-﻿namespace Day03
+﻿namespace Day03;
+
+public record Person(string FirstName, string LastName, params Pet[] Pets);
+
+public record Pet(PetType Type, string Name, int Age);
+
+public enum PetType
 {
-    public record Person(string FirstName, string LastName, params Pet[] Pets);
-
-    public record Pet(PetType Type, string Name, int Age);
-
-    public enum PetType
-    {
-        Cat,
-        Dog,
-        Hamster,
-        Turtle,
-        Bird,
-        Snake
-    }
+    Cat,
+    Dog,
+    Hamster,
+    Turtle,
+    Bird,
+    Snake
 }
