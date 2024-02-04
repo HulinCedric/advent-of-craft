de7b6f34c4219303ff9cc0f8b01fb6ecf85f49d7
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 17:48:09 +0100

    refactor: Encapsulate number representation

--- exercise/c#/Day02/Day02/FizzBuzz.cs
+++ exercise/c#/Day02/Day02/FizzBuzz.cs
@@ -9,15 +9,18 @@ public static class FizzBuzz
             _ when number.IsDivisibleBy(3) && number.IsDivisibleBy(5) => "FizzBuzz",
             _ when number.IsDivisibleBy(3) => "Fizz",
             _ when number.IsDivisibleBy(5) => "Buzz",
-            _ => number.ToString()
+            _ => number.Representation()
         };
 
     private static bool IsOutOfRange(this int number)
         => number is <= 0 or > 100;
 
+    private static string ToFailure()
+        => throw new OutOfRangeException();
+
     private static bool IsDivisibleBy(this int number, int divisor)
         => number % divisor == 0;
 
-    private static string ToFailure()
-        => throw new OutOfRangeException();
+    private static string Representation(this int number)
+        => number.ToString();
 }
