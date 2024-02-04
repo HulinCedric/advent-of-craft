ce99a97ff7da8bf6cfe66b1cc868bbd7286f3afc
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 17:19:21 +0100

    refactor: Remove else

--- exercise/c#/Day02/Day02/FizzBuzz.cs
+++ exercise/c#/Day02/Day02/FizzBuzz.cs
@@ -8,32 +8,28 @@ public static class FizzBuzz
         {
             throw new OutOfRangeException();
         }
-        else
+
+        if (input > 100)
         {
-            if (input > 100)
-            {
-                throw new OutOfRangeException();
-            }
-            else
-            {
-                if (input % 3 == 0 &&
-                    input % 5 == 0)
-                {
-                    return "FizzBuzz";
-                }
-
-                if (input % 3 == 0)
-                {
-                    return "Fizz";
-                }
-
-                if (input % 5 == 0)
-                {
-                    return "Buzz";
-                }
-
-                return input.ToString();
-            }
+            throw new OutOfRangeException();
         }
+
+        if (input % 3 == 0 &&
+            input % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+
+        if (input % 3 == 0)
+        {
+            return "Fizz";
+        }
+
+        if (input % 5 == 0)
+        {
+            return "Buzz";
+        }
+
+        return input.ToString();
     }
 }
