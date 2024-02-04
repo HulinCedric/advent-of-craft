e2895aae1fd0f6e90d34e5ae36b14c7e5dd1999d
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 18:41:29 +0100

    refactor: YoungestPetIfAny

--- exercise/c#/Day03/Day03/Person.cs
+++ exercise/c#/Day03/Day03/Person.cs
@@ -4,9 +4,12 @@ public record Person(string FirstName, string LastName, params Pet[] Pets)
 {
     public int YoungestPetAge()
     {
-        var youngestPetByAge = Pets.MinBy(p => p.Age);
+        var youngestPetByAge = YoungestPetIfAny();
         return youngestPetByAge?.Age ?? int.MaxValue;
     }
+
+    private Pet? YoungestPetIfAny()
+        => Pets.MinBy(p => p.Age);
 }
 
 public record Pet(PetType Type, string Name, int Age);
