3450607d6805a89d52d2e0311fed32dbbf2965c1
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 17:44:32 +0100

    refactor: Encapsulate out of range

--- exercise/c#/Day02/Day02/FizzBuzz.cs
+++ exercise/c#/Day02/Day02/FizzBuzz.cs
@@ -5,13 +5,15 @@ public static class FizzBuzz
     public static string Convert(int number)
         => number switch
         {
-            <= 0 => throw new OutOfRangeException(),
-            > 100 => throw new OutOfRangeException(),
+            _ when number.IsOutOfRange() => throw new OutOfRangeException(),
             _ when number.IsDivisibleBy(3) && number.IsDivisibleBy(5) => "FizzBuzz",
             _ when number.IsDivisibleBy(3) => "Fizz",
             _ when number.IsDivisibleBy(5) => "Buzz",
             _ => number.ToString()
         };
+
+    private static bool IsOutOfRange(this int number)
+        => number is <= 0 or > 100;
 
     private static bool IsDivisibleBy(this int number, int divisor)
         => number % divisor == 0;
