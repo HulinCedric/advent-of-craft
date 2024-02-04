62db0d330ab58583732225e630f33ca3ff8533bf
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 18:50:22 +0100

    refactor: Inline and simplify age need

--- exercise/c#/Day03/Day03/Person.cs
+++ exercise/c#/Day03/Day03/Person.cs
@@ -3,10 +3,7 @@
 public record Person(string FirstName, string LastName, params Pet[] Pets)
 {
     public int YoungestPetAge()
-        => YoungestPetIfAny()?.Age ?? int.MaxValue;
-
-    private Pet? YoungestPetIfAny()
-        => Pets.MinBy(p => p.Age);
+        => Pets.Any() ? Pets.Min(pet => pet.Age) : int.MaxValue;
 }
 
 public record Pet(PetType Type, string Name, int Age);
