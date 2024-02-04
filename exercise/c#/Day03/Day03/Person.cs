51281a867ea23679715c1ac3b9043ccb9217e071
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 19:12:37 +0100

    refactor: Encapsulate HasPets

--- exercise/c#/Day03/Day03/Person.cs
+++ exercise/c#/Day03/Day03/Person.cs
@@ -3,7 +3,10 @@
 public record Person(string FirstName, string LastName, params Pet[] Pets)
 {
     public int YoungestPetAge()
-        => Pets.Any() ? Pets.Min(pet => pet.Age) : int.MaxValue;
+        => HasPets() ? Pets.Min(pet => pet.Age) : int.MaxValue;
+
+    private bool HasPets()
+        => Pets.Any();
 }
 
 public record Pet(PetType Type, string Name, int Age);
