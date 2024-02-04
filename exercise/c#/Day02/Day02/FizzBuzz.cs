e112fa07f08af52adf19b9b9908ea00ab6891e81
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-04 17:45:05 +0100

    refactor: To Faillure

--- exercise/c#/Day02/Day02/FizzBuzz.cs
+++ exercise/c#/Day02/Day02/FizzBuzz.cs
@@ -5,7 +5,7 @@ public static class FizzBuzz
     public static string Convert(int number)
         => number switch
         {
-            _ when number.IsOutOfRange() => throw new OutOfRangeException(),
+            _ when number.IsOutOfRange() => ToFailure(),
             _ when number.IsDivisibleBy(3) && number.IsDivisibleBy(5) => "FizzBuzz",
             _ when number.IsDivisibleBy(3) => "Fizz",
             _ when number.IsDivisibleBy(5) => "Buzz",
@@ -17,4 +17,7 @@ public static class FizzBuzz
 
     private static bool IsDivisibleBy(this int number, int divisor)
         => number % divisor == 0;
+
+    private static string ToFailure()
+        => throw new OutOfRangeException();
 }
