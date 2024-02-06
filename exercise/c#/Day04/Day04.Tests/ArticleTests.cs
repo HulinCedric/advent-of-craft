8c581264735530e30dbd3a95b3c324ffb786921b
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-06 22:06:51 +0100

    refactor: Add missing test case

--- exercise/c#/Day04/Day04.Tests/ArticleTests.cs
+++ exercise/c#/Day04/Day04.Tests/ArticleTests.cs
@@ -7,21 +7,34 @@ public class ArticleTests
 {
     private const string CommentText = "Amazing article !!!";
     private const string CommentAuthor = "Pablo Escobar";
-    
+
     private readonly Article _article = new(
         "Lorem Ipsum",
-        "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-    );
+        "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore");
 
     [Fact]
     public void Should_add_comment()
     {
         var today = DateOnly.FromDateTime(DateTime.Today);
-       
+
         _article.AddComment(CommentText, CommentAuthor);
 
         _article.ShouldHaveCommentsCount(1);
         _article.ShouldHaveCommentWith(CommentText, CommentAuthor, today);
+    }
+
+    [Fact]
+    public void Should_add_comment_in_an_article_already_containing_a_comment()
+    {
+        const string secondCommentText = "Lorem Ipsum";
+        const string secondCommentAuthor = "Ipsum Lorem";
+        var today = DateOnly.FromDateTime(DateTime.Today);
+
+        _article.AddComment(CommentText, CommentAuthor);
+        _article.AddComment(secondCommentText, secondCommentAuthor);
+
+        _article.ShouldHaveCommentsCount(2);
+        _article.ShouldHaveCommentWith(secondCommentText, secondCommentAuthor, today);
     }
 
     [Fact]
