2b88d117966355c04c1c13249ab2d514923649fe
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-06 21:30:14 +0100

    refactor: Merge aspect of Add Comment behavior
    It_Should_Add_A_Comment_With_The_Date_Of_The_Day

--- exercise/c#/Day04/Day04.Tests/ArticleTests.cs
+++ exercise/c#/Day04/Day04.Tests/ArticleTests.cs
@@ -8,15 +8,17 @@ public class ArticleTests
     [Fact]
     // It_Should_Add_A_Comment_With_The_Given_Text
     // It_Should_Add_A_Comment_With_The_Given_Author
+    // It_Should_Add_A_Comment_With_The_Date_Of_The_Day
     public void It_Should_Add_Valid_Comment()
     {
         var article = new Article(
             "Lorem Ipsum",
             "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
         );
-
         const string text = "Amazing article !!!";
         const string author = "Pablo Escobar";
+        var today = DateOnly.FromDateTime(DateTime.Today);
+       
         article.AddComment(text, author);
         
         article.Comments
@@ -25,19 +27,6 @@ public class ArticleTests
         article.Comments
             .Should().HaveCount(1)
             .And.ContainSingle(comment => comment.Author == author);
-    }
-    
-    [Fact]
-    public void It_Should_Add_A_Comment_With_The_Date_Of_The_Day()
-    {
-        var article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
-        article.AddComment("Amazing article !!!", "Pablo Escobar");
-
-        var today = DateOnly.FromDateTime(DateTime.Today);
         article.Comments
             .Should().ContainSingle(comment => comment.CreationDate == today);
     }
