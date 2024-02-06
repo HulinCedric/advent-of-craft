c8e966dfbf7a6a1a4a38781dc95d45b672c0bb1d
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-06 21:46:23 +0100

    refactor: Article field

--- exercise/c#/Day04/Day04.Tests/ArticleTests.cs
+++ exercise/c#/Day04/Day04.Tests/ArticleTests.cs
@@ -5,35 +5,32 @@ namespace Day04.Tests;
 
 public class ArticleTests
 {
+    private readonly Article _article = new(
+        "Lorem Ipsum",
+        "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+    );
+
     [Fact]
     // It_Should_Add_A_Comment_With_The_Given_Text
     // It_Should_Add_A_Comment_With_The_Given_Author
     // It_Should_Add_A_Comment_With_The_Date_Of_The_Day
     public void It_Should_Add_Valid_Comment()
     {
-        var article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
         const string text = "Amazing article !!!";
         const string author = "Pablo Escobar";
         var today = DateOnly.FromDateTime(DateTime.Today);
        
-        article.AddComment(text, author);
+        _article.AddComment(text, author);
 
-        article.ShouldHaveCommentWith(text, author, today);
+        _article.ShouldHaveCommentWith(text, author, today);
     }
 
     [Fact]
     public void It_Should_Throw_An_Exception_When_Adding_Existing_Comment()
     {
-        var article = new Article(
-            "Lorem Ipsum",
-            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-        article.AddComment("Amazing article !!!", "Pablo Escobar");
+        _article.AddComment("Amazing article !!!", "Pablo Escobar");
 
-        var act = () => article.AddComment("Amazing article !!!", "Pablo Escobar");
+        var act = () => _article.AddComment("Amazing article !!!", "Pablo Escobar");
         act.Should().Throw<CommentAlreadyExistException>();
     }
 }
