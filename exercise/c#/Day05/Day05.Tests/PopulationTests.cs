fff99828029f5bda248684cd1d7e94510c70e4a3
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-10 16:43:21 +0100

    refactor: File scoped namespace

--- exercise/c#/Day05/Day05.Tests/PopulationTests.cs
+++ exercise/c#/Day05/Day05.Tests/PopulationTests.cs
@@ -4,83 +4,82 @@ using Xunit;
 using static System.Environment;
 using static Day05.PetType;
 
-namespace Day05.Tests
+namespace Day05.Tests;
+
+public class PopulationTests
 {
-    public class PopulationTests
+    private static readonly IEnumerable<Person> Population = new List<Person>
     {
-        private static readonly IEnumerable<Person> Population = new List<Person>
+        new("Peter", "Griffin", new Pet(Cat, "Tabby", 2)),
+        new("Stewie", "Griffin", new Pet(Cat, "Dolly", 3), new Pet(Dog, "Brian", 9)),
+        new("Joe", "Swanson", new Pet(Dog, "Spike", 4)),
+        new("Lois", "Griffin", new Pet(Snake, "Serpy", 1)),
+        new("Meg", "Griffin", new Pet(Bird, "Tweety", 1)),
+        new("Chris", "Griffin", new Pet(Turtle, "Speedy", 4)),
+        new("Cleveland", "Brown", new Pet(Hamster, "Fuzzy", 1), new Pet(Hamster, "Wuzzy", 2)),
+        new("Glenn", "Quagmire")
+    };
+
+    [Fact]
+    public void People_With_Their_Pets()
+    {
+        var response = FormatPopulation();
+
+        response.ToString()
+            .Should()
+            .Be("""
+                Peter Griffin who owns : Tabby
+                Stewie Griffin who owns : Dolly Brian
+                Joe Swanson who owns : Spike
+                Lois Griffin who owns : Serpy
+                Meg Griffin who owns : Tweety
+                Chris Griffin who owns : Speedy
+                Cleveland Brown who owns : Fuzzy Wuzzy
+                Glenn Quagmire
+                """);
+    }
+
+    private static StringBuilder FormatPopulation()
+    {
+        var response = new StringBuilder();
+
+        foreach (var person in Population)
         {
-            new("Peter", "Griffin", new Pet(Cat, "Tabby", 2)),
-            new("Stewie", "Griffin", new Pet(Cat, "Dolly", 3), new Pet(Dog, "Brian", 9)),
-            new("Joe", "Swanson", new Pet(Dog, "Spike", 4)),
-            new("Lois", "Griffin", new Pet(Snake, "Serpy", 1)),
-            new("Meg", "Griffin", new Pet(Bird, "Tweety", 1)),
-            new("Chris", "Griffin", new Pet(Turtle, "Speedy", 4)),
-            new("Cleveland", "Brown", new Pet(Hamster, "Fuzzy", 1), new Pet(Hamster, "Wuzzy", 2)),
-            new("Glenn", "Quagmire")
-        };
+            response.Append($"{person.FirstName} {person.LastName}");
 
-        [Fact]
-        public void People_With_Their_Pets()
-        {
-            var response = FormatPopulation();
-
-            response.ToString()
-                .Should()
-                .Be("""
-                    Peter Griffin who owns : Tabby
-                    Stewie Griffin who owns : Dolly Brian
-                    Joe Swanson who owns : Spike
-                    Lois Griffin who owns : Serpy
-                    Meg Griffin who owns : Tweety
-                    Chris Griffin who owns : Speedy
-                    Cleveland Brown who owns : Fuzzy Wuzzy
-                    Glenn Quagmire
-                    """);
-        }
-
-        private static StringBuilder FormatPopulation()
-        {
-            var response = new StringBuilder();
-
-            foreach (var person in Population)
+            if (person.Pets.Length > 0)
             {
-                response.Append($"{person.FirstName} {person.LastName}");
+                response.Append(" who owns : ");
+            }
 
-                if (person.Pets.Length > 0)
+            foreach (var pet in person.Pets)
+            {
+                response.Append($"{pet.Name}");
+
+                if (pet != person.Pets.Last())
                 {
-                    response.Append(" who owns : ");
-                }
-
-                foreach (var pet in person.Pets)
-                {
-                    response.Append($"{pet.Name}");
-
-                    if (pet != person.Pets.Last())
-                    {
-                        response.Append(' ');
-                    }
-                }
-
-                if (person != Population.Last())
-                {
-                    response.Append(NewLine);
+                    response.Append(' ');
                 }
             }
 
-            return response;
+            if (person != Population.Last())
+            {
+                response.Append(NewLine);
+            }
         }
 
-        [Fact]
-        public void Who_Owns_The_Youngest_Pet()
-        {
-            var filtered = Population.MinBy(YoungestPetAgeOfThePerson);
-
-            filtered.Should().NotBeNull();
-            filtered!.FirstName.Should().Be("Lois");
-        }
-
-        private static int YoungestPetAgeOfThePerson(Person person) =>
-            person.Pets.MinBy(p => p.Age)?.Age ?? int.MaxValue;
+        return response;
     }
+
+    [Fact]
+    public void Who_Owns_The_Youngest_Pet()
+    {
+        var filtered = Population.MinBy(YoungestPetAgeOfThePerson);
+
+        filtered.Should().NotBeNull();
+        filtered!.FirstName.Should().Be("Lois");
+    }
+
+    private static int YoungestPetAgeOfThePerson(Person person) =>
+        person.Pets.MinBy(p => p.Age)?.Age ?? int.MaxValue;
 }
