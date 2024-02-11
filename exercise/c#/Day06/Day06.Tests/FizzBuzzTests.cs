eddaff6aff4ec677dfb1fc55e1721764e5e38919
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-11 17:06:32 +0100

    refactor: Convert 50 case into parameter

--- exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
+++ exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
@@ -29,10 +29,8 @@ public class FizzBuzzTests
 
     [Theory]
     [InlineData(5)]
+    [InlineData(50)]
     public void Returns_Buzz_For_(int input) => FizzBuzz.Convert(input).Should().Be("Buzz");
-
-    [Fact]
-    public void Returns_Buzz_For_50() => FizzBuzz.Convert(50).Should().Be("Buzz");
 
     [Fact]
     public void Returns_Buzz_For_85() => FizzBuzz.Convert(85).Should().Be("Buzz");
