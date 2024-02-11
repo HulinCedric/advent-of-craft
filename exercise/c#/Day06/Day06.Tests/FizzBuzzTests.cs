fa33bf902db1c805ee0161f0e6e3fa7062306260
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-11 17:16:11 +0100

    refactor: Remove regions

--- exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
+++ exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
@@ -5,17 +5,11 @@ namespace Day06.Tests;
 
 public class FizzBuzzTests
 {
-    #region "Normal" numbers
-
     [Theory]
     [InlineData(1, "1")]
     [InlineData(67, "67")]
     [InlineData(82, "82")]
     public void Returns_The_Given_Number_For_(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
-    
-    #endregion
-
-    #region Fizz
 
     [Theory]
     [InlineData(3)]
@@ -23,29 +17,17 @@ public class FizzBuzzTests
     [InlineData(99)]
     public void Returns_Fizz_For_(int input) => FizzBuzz.Convert(input).Should().Be("Fizz");
 
-    #endregion
-
-    #region Buzz
-
     [Theory]
     [InlineData(5)]
     [InlineData(50)]
     [InlineData(85)]
     public void Returns_Buzz_For_(int input) => FizzBuzz.Convert(input).Should().Be("Buzz");
 
-    #endregion
-
-    #region FizzBuzz
-
     [Theory]
     [InlineData(15)]
     [InlineData(30)]
     [InlineData(45)]
     public void Returns_FizzBuzz_For_(int input) => FizzBuzz.Convert(input).Should().Be("FizzBuzz");
-
-    #endregion
-
-    #region Failures
 
     [Theory]
     [InlineData(0)]
@@ -56,6 +38,4 @@ public class FizzBuzzTests
         var act = () => FizzBuzz.Convert(input);
         act.Should().Throw<OutOfRangeException>();
     }
-
-    #endregion
 }
