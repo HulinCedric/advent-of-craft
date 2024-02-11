ac2781474e7a1e2d555cf102163e0840fb15e4c0
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-11 17:19:44 +0100

    refactor: Merge by behavior

--- exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
+++ exercise/c#/Day06/Day06.Tests/FizzBuzzTests.cs
@@ -6,29 +6,24 @@ namespace Day06.Tests;
 public class FizzBuzzTests
 {
     [Theory]
+    
     [InlineData(1, "1")]
     [InlineData(67, "67")]
     [InlineData(82, "82")]
-    public void Returns_The_Given_Number_For_(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
-
-    [Theory]
+    
     [InlineData(3, "Fizz")]
     [InlineData(66, "Fizz")]
     [InlineData(99, "Fizz")]
-    public void Returns_Fizz_For_(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
-
-    [Theory]
+    
     [InlineData(5, "Buzz")]
     [InlineData(50, "Buzz")]
     [InlineData(85, "Buzz")]
-    public void Returns_Buzz_For_(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
-
-    [Theory]
+    
     [InlineData(15, "FizzBuzz")]
     [InlineData(30, "FizzBuzz")]
     [InlineData(45, "FizzBuzz")]
-    public void Returns_FizzBuzz_For_(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
-
+    public void Returns_Number_Conversion(int input, string output) => FizzBuzz.Convert(input).Should().Be(output);
+    
     [Theory]
     [InlineData(0)]
     [InlineData(101)]
