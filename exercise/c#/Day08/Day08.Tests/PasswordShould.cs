1768a447a16a7967a225d3c0edcacc62fab7c85e
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-12 21:57:45 +0100

    test: Contains at least one lowercase letter

--- exercise/c#/Day08/Day08.Tests/PasswordShould.cs
+++ exercise/c#/Day08/Day08.Tests/PasswordShould.cs
@@ -13,9 +13,10 @@ public class PasswordShould
     public void Be_invalid_when_contains_no_capital_letter()
         => Password.IsValid("12345678").Should().BeFalse();
 
+    // TODO Contains at least one lowercase letter
     [Fact]
-    public void Be_valid()
-        => Password.IsValid("A2345678").Should().BeTrue();
+    public void Be_invalid_when_contains_no_lowercase_letter()
+        => Password.IsValid("A2345678").Should().BeFalse();
 
     // TODO Contains at least a number
     // TODO Contains at least a special character in this list . * # @ $ % &.
@@ -27,7 +28,8 @@ public class Password
     private const int MaxLength = 8;
 
     public static bool IsValid(string password)
-        => IsLessOrEqualsTo(password, MaxLength) &&
+        => false &&
+            IsLessOrEqualsTo(password, MaxLength) &&
            ContainsAtLeastOneCapitalLetter(password);
 
     private static bool IsLessOrEqualsTo(string password, int maxLength)
