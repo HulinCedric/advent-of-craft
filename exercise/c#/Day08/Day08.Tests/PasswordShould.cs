3fed41d3117623a3749becfb3b29e8e4d5f8531d
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-13 09:20:24 +0100

    refactor: File by classes

--- exercise/c#/Day08/Day08.Tests/PasswordShould.cs
+++ exercise/c#/Day08/Day08.Tests/PasswordShould.cs
@@ -15,7 +15,7 @@ public class PasswordShould
     [InlineData("Aa1Cc2Dd3.)", "Unauthorized character")]
     [InlineData("Aa1Cc2Dd3./", "Unauthorized character")]
     public void Be_invalid(string password, string reason)
-        => Password.IsValid(password).Should().BeFalse(reason);
+        => PasswordValidator.IsValid(password).Should().BeFalse(reason);
 
     [Theory]
     [InlineData("Aa1Cc2Dd3.")]
@@ -26,37 +26,5 @@ public class PasswordShould
     [InlineData("Aa1Cc2Dd3%")]
     [InlineData("Aa1Cc2Dd3&")]
     public void Be_valid(string password)
-        => Password.IsValid(password).Should().BeTrue();
-}
-
-public class Password
-{
-    private const int MinimumLength = 8;
-    private const string SpecialCharacters = ".*#@$%&";
-
-    public static bool IsValid(string password)
-        => HasLengthGreaterOrEqualsToMinimumLength(password) &&
-           ContainsAtLeastOneCapitalLetter(password) &&
-           ContainsAtLeastOneLowercaseLetter(password) &&
-           ContainsAtLeastANumber(password) &&
-           ContainsAtLeastASpecialCharacter(password) &&
-           ContainsOnlyAuthorizedCharacters(password);
-
-    private static bool ContainsOnlyAuthorizedCharacters(string password)
-        => password.All(c => char.IsLetterOrDigit(c) || SpecialCharacters.Contains(c));
-
-    private static bool ContainsAtLeastASpecialCharacter(string password)
-        => password.Any(SpecialCharacters.Contains);
-
-    private static bool ContainsAtLeastANumber(string password)
-        => password.Any(char.IsDigit);
-
-    private static bool HasLengthGreaterOrEqualsToMinimumLength(string password)
-        => password.Length >= MinimumLength;
-
-    private static bool ContainsAtLeastOneCapitalLetter(string password)
-        => password.Any(char.IsUpper);
-
-    private static bool ContainsAtLeastOneLowercaseLetter(string password)
-        => password.Any(char.IsLower);
+        => PasswordValidator.IsValid(password).Should().BeTrue();
 }
---
+++ exercise/c#/Day08/Day08/PasswordValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace Day08;
+
+public static class PasswordValidator
+{
+    private const int MinimumLength = 8;
+    private const string SpecialCharacters = ".*#@$%&";
+
+    public static bool IsValid(string password)
+        => HasLengthGreaterOrEqualsToMinimumLength(password) &&
+           ContainsAtLeastOneCapitalLetter(password) &&
+           ContainsAtLeastOneLowercaseLetter(password) &&
+           ContainsAtLeastANumber(password) &&
+           ContainsAtLeastASpecialCharacter(password) &&
+           ContainsOnlyAuthorizedCharacters(password);
+
+    private static bool ContainsOnlyAuthorizedCharacters(string password)
+        => password.All(c => char.IsLetterOrDigit(c) || SpecialCharacters.Contains(c));
+
+    private static bool ContainsAtLeastASpecialCharacter(string password)
+        => password.Any(SpecialCharacters.Contains);
+
+    private static bool ContainsAtLeastANumber(string password)
+        => password.Any(char.IsDigit);
+
+    private static bool HasLengthGreaterOrEqualsToMinimumLength(string password)
+        => password.Length >= MinimumLength;
+
+    private static bool ContainsAtLeastOneCapitalLetter(string password)
+        => password.Any(char.IsUpper);
+
+    private static bool ContainsAtLeastOneLowercaseLetter(string password)
+        => password.Any(char.IsLower);
+}
