0cac1e63a6ed9aeba503899e9bc1eec19f0abb71
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-13 09:30:22 +0100

    refactor: Rules extensions

--- exercise/c#/Day08/Day08/PasswordValidator.cs
+++ exercise/c#/Day08/Day08/PasswordValidator.cs
@@ -1,39 +1,14 @@
-﻿namespace Day08;
+﻿using static Day08.PasswordValidatorRules;
+
+namespace Day08;
 
 public static class PasswordValidator
 {
-    private const int MinimumLength = 8;
-    private const string SpecialCharacters = ".*#@$%&";
-
     public static bool IsValid(string password)
-        => HasLengthGreaterOrEqualsToMinimumLength(password) &&
-           ContainsAtLeastOne(password, CapitalLetter()) &&
-           ContainsAtLeastOne(password, LowercaseLetter()) &&
-           ContainsAtLeastOne(password, Number()) &&
-           ContainsAtLeastOne(password, SpecialCharacter()) &&
-           ContainsOnly(password, AuthorizedCharacters());
-
-    private static bool HasLengthGreaterOrEqualsToMinimumLength(string password)
-        => password.Length >= MinimumLength;
-
-    private static bool ContainsAtLeastOne(string password, Func<char, bool> predicate)
-        => password.Any(predicate);
-
-    private static bool ContainsOnly(string password, Func<char, bool> predicate)
-        => password.All(predicate);
-
-    private static Func<char, bool> CapitalLetter()
-        => char.IsUpper;
-
-    private static Func<char, bool> LowercaseLetter()
-        => char.IsLower;
-
-    private static Func<char, bool> Number()
-        => char.IsDigit;
-
-    private static Func<char, bool> SpecialCharacter()
-        => SpecialCharacters.Contains;
-
-    private static Func<char, bool> AuthorizedCharacters()
-        => c => char.IsLetterOrDigit(c) || SpecialCharacters.Contains(c);
+        => password.HasLengthGreaterOrEqualsToMinimumLength() &&
+           password.ContainsAtLeastOne(CapitalLetter()) &&
+           password.ContainsAtLeastOne(LowercaseLetter()) &&
+           password.ContainsAtLeastOne(Number()) &&
+           password.ContainsAtLeastOne(SpecialCharacter()) &&
+           password.ContainsOnly(AuthorizedCharacters());
 }
---
+++ exercise/c#/Day08/Day08/PasswordValidatorRules.cs
@@ -0,0 +1,31 @@
+﻿namespace Day08;
+
+internal static class PasswordValidatorRules
+{
+    internal const int MinimumLength = 8;
+    internal const string SpecialCharacters = ".*#@$%&";
+
+    internal static bool HasLengthGreaterOrEqualsToMinimumLength(this string password)
+        => password.Length >= MinimumLength;
+
+    internal static bool ContainsAtLeastOne(this string password, Func<char, bool> predicate)
+        => password.Any(predicate);
+
+    internal static bool ContainsOnly(this string password, Func<char, bool> predicate)
+        => password.All(predicate);
+
+    internal static Func<char, bool> CapitalLetter()
+        => char.IsUpper;
+
+    internal static Func<char, bool> LowercaseLetter()
+        => char.IsLower;
+
+    internal static Func<char, bool> Number()
+        => char.IsDigit;
+
+    internal static Func<char, bool> SpecialCharacter()
+        => SpecialCharacters.Contains;
+
+    internal static Func<char, bool> AuthorizedCharacters()
+        => c => char.IsLetterOrDigit(c) || SpecialCharacters.Contains(c);
+}
