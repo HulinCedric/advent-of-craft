ca521777b0b8e2e19eb60280b896ec1ceb0b9ebc
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-26 16:44:24 +0100

    refactor: Extract greeting in methods

--- exercise/c#/Day12/Day12/Greeter.cs
+++ exercise/c#/Day12/Day12/Greeter.cs
@@ -7,9 +7,21 @@ public class Greeter
     public string Greet()
         => Formality switch
         {
-            "formal" => "Good evening, sir.",
-            "casual" => "Sup bro?",
-            "intimate" => "Hello Darling!",
-            _ => "Hello."
+            "formal" => FormalGreeting(),
+            "casual" => CasualGreeting(),
+            "intimate" => IntimateGreeting(),
+            _ => DefaultGreeting()
         };
+
+    private static string DefaultGreeting()
+        => "Hello.";
+
+    private static string IntimateGreeting()
+        => "Hello Darling!";
+
+    private static string CasualGreeting()
+        => "Sup bro?";
+
+    private static string FormalGreeting()
+        => "Good evening, sir.";
 }
