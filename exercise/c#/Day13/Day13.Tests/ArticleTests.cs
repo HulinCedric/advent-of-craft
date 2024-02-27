917f56e1e1a7f6374e77841087f6185cd665a1b4
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-27 18:58:21 +0100

    Inline article field to use test builder

--- exercise/c#/Day13/Day13.Tests/ArticleTests.cs
+++ exercise/c#/Day13/Day13.Tests/ArticleTests.cs
@@ -19,14 +19,13 @@ public class ArticleTests
     private const string Author = "Pablo Escobar";
     private const string CommentText = "Amazing article !!!";
 
-    private readonly Article _article = ArticleTestBuilder.Article().Build();
-
     [Fact]
     public void Should_Add_Comment_In_An_Article()
     {
-        _article.AddComment(CommentText, Author);
+        var article = ArticleTestBuilder.Article().Build();
+        article.AddComment(CommentText, Author);
 
-        _article.Comments
+        article.Comments
             .Should()
             .HaveCount(1)
             .And.ContainSingle(comment => comment.Text == CommentText && comment.Author == Author);
@@ -38,12 +37,13 @@ public class ArticleTests
         const string newComment = "Finibus Bonorum et Malorum";
         const string newAuthor = "Al Capone";
 
-        _article.AddComment(CommentText, Author);
-        _article.AddComment(newComment, newAuthor);
+        var article = ArticleTestBuilder.Article().Build();
+        article.AddComment(CommentText, Author);
+        article.AddComment(newComment, newAuthor);
 
-        _article.Comments.Should().HaveCount(2);
+        article.Comments.Should().HaveCount(2);
 
-        var lastComment = _article.Comments.Last();
+        var lastComment = article.Comments.Last();
         lastComment.Text.Should().Be(newComment);
         lastComment.Author.Should().Be(newAuthor);
     }
@@ -53,9 +53,10 @@ public class ArticleTests
         [Fact]
         public void When_Adding_An_Existing_Comment()
         {
-            _article.AddComment(CommentText, Author);
+            var article = ArticleTestBuilder.Article().Build();
+            article.AddComment(CommentText, Author);
 
-            var act = () => _article.AddComment(CommentText, Author);
+            var act = () => article.AddComment(CommentText, Author);
             act.Should().Throw<CommentAlreadyExistException>();
         }
     }
