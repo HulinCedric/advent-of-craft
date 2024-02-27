e9b8984d1fcbe50e70b0854fddd0ffc12a6f6739
Cédric Hulin <hulin.cedric@gmail.com>
2024-02-27 19:51:50 +0100

    Given When Then DSL

--- exercise/c#/Day13/Day13.Tests/ArticleTests.cs
+++ exercise/c#/Day13/Day13.Tests/ArticleTests.cs
@@ -6,15 +6,19 @@ namespace Day13.Tests;
 
 public class ArticleTests
 {
+    private Article _article;
+
     [Fact]
     public void Should_Add_Comment_In_An_Article()
     {
-        var article = AnArticle().Build();
-
-        article.AddComment(CommentText, Author);
-
-        article.Comments.Should().HaveCount(1);
-        AssertComment(article.Comments.Last(), CommentText, Author);
+        Given(AnArticle());
+        When(article => article.AddComment(CommentText, Author));
+        Then(
+            article =>
+            {
+                article.Comments.Should().HaveCount(1);
+                AssertComment(article.Comments.Last(), CommentText, Author);
+            });
     }
 
     [Fact]
@@ -23,14 +27,14 @@ public class ArticleTests
         const string newComment = "Finibus Bonorum et Malorum";
         const string newAuthor = "Al Capone";
 
-        var article = AnArticle()
-            .Commented()
-            .Build();
-
-        article.AddComment(newComment, newAuthor);
-
-        article.Comments.Should().HaveCount(2);
-        AssertComment(article.Comments.Last(), newComment, newAuthor);
+        Given(AnArticle().Commented());
+        When(article => article.AddComment(newComment, newAuthor));
+        Then(
+            article =>
+            {
+                article.Comments.Should().HaveCount(2);
+                AssertComment(article.Comments[1], newComment, newAuthor);
+            });
     }
 
     private static void AssertComment(Comment comment, string expectedComment, string expectedAuthor)
@@ -40,13 +44,21 @@ public class ArticleTests
         comment.CreationDate.Should().Be(DateOnly.FromDateTime(DateTime.Now));
     }
 
+    private void Given(ArticleTestBuilder articleBuilder)
+        => _article = articleBuilder.Build();
+
+    private void When(Action<Article> act)
+        => act(_article);
+
+    private void Then(Action<Article> act)
+        => act(_article);
+
     public class Fail : ArticleTests
     {
         [Fact]
         public void When_Adding_An_Existing_Comment()
         {
             var article = AnArticle().Build();
-
             article.AddComment(CommentText, Author);
 
             var act = () => article.AddComment(CommentText, Author);
