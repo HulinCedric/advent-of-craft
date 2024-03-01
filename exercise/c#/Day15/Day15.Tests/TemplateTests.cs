fb6e675965753bbbd198d041dc0501579c09b23a
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-01 21:51:22 +0100

    test: Harness discovery

---
+++ exercise/c#/Day15/Day15.Tests/TemplateTests.cs
@@ -0,0 +1,48 @@
+namespace Day15.Tests;
+
+[UsesVerify]
+public class TemplateTests
+{
+    private static readonly List<string> DocumentTypes =
+    [
+        "DEER",
+        "AUTP",
+        "AUTM",
+        "SPEC",
+        "GLPP",
+        "GLPM"
+    ];
+
+    private static readonly string[] RecordTypes = Enum.GetNames<RecordType>();
+
+    [Fact]
+    public Task VerifyAllCombinations()
+        => Verify(AllCombinationResults());
+
+    private static IEnumerable<string> AllCombinationResults()
+    {
+        foreach (var (documentType, recordType) in AllCombinations())
+        {
+            var result = FindTemplateSafely(documentType, recordType);
+
+            yield return $"[{documentType}, {recordType}] => {result}";
+        }
+    }
+
+    private static IEnumerable<(string documentType, string recordType)> AllCombinations()
+        => from documentType in DocumentTypes
+           from recordType in RecordTypes
+           select (documentType, recordType);
+
+    private static string FindTemplateSafely(string documentType, string recordType)
+    {
+        try
+        {
+            return $"{Template.FindTemplateFor(documentType, recordType)}";
+        }
+        catch (Exception ex)
+        {
+            return $"{ex.GetType()}: {ex.Message}";
+        }
+    }
+}
