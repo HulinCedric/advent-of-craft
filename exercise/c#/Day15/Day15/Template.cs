224b21df1c71e082460aa9f3fd9abe40b0e067be
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-02 08:53:49 +0100

    refactor: Use DocumenType enum in production code

--- exercise/c#/Day15/Day15/Template.cs
+++ exercise/c#/Day15/Day15/Template.cs
@@ -3,29 +3,29 @@ namespace Day15
     public record Template(
         DocumentTemplate DocumentTemplate,
         RecordType RecordType,
-        string DocumentType)
+        DocumentType DocumentType)
     {
         private static IEnumerable<Template> TemplateMappings() => new[]
         {
-            new Template(DocumentTemplate.DEERPP, RecordType.IndividualProspect, "DEER"),
-            new Template(DocumentTemplate.DEERPM, RecordType.LegalProspect, "DEER"),
-            new Template(DocumentTemplate.AUTP, RecordType.IndividualProspect, "AUTP"),
-            new Template(DocumentTemplate.AUTM, RecordType.LegalProspect, "AUTM"),
-            new Template(DocumentTemplate.SPEC, RecordType.All, "SPEC"),
-            new Template(DocumentTemplate.GLPP, RecordType.IndividualProspect, "GLPP"),
-            new Template(DocumentTemplate.GLPM, RecordType.LegalProspect, "GLPM")
+            new Template(DocumentTemplate.DEERPP, RecordType.IndividualProspect, DocumentType.DEER),
+            new Template(DocumentTemplate.DEERPM, RecordType.LegalProspect, DocumentType.DEER),
+            new Template(DocumentTemplate.AUTP, RecordType.IndividualProspect, DocumentType.AUTP),
+            new Template(DocumentTemplate.AUTM, RecordType.LegalProspect, DocumentType.AUTM),
+            new Template(DocumentTemplate.SPEC, RecordType.All, DocumentType.SPEC),
+            new Template(DocumentTemplate.GLPP, RecordType.IndividualProspect, DocumentType.GLPP),
+            new Template(DocumentTemplate.GLPM, RecordType.LegalProspect, DocumentType.GLPM)
         };
 
         public static Template FindTemplateFor(string documentType, string recordType)
         {
             foreach (var dtt in TemplateMappings())
             {
-                if (dtt.DocumentType.Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
+                if (dtt.DocumentType.ToString().Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
                     dtt.RecordType.ToString().Equals(recordType, StringComparison.InvariantCultureIgnoreCase))
                 {
                     return dtt;
                 }
-                else if (dtt.DocumentType.Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
+                else if (dtt.DocumentType.ToString().Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
                          dtt.RecordType.ToString() == "All")
                 {
                     return dtt;
