d8137662b166e1dae477faaea8e8599f26ac3e1c
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-02 19:22:52 +0100

    refactor: Use ImmutableList

--- exercise/c#/Day16/Day16/Article.cs
+++ exercise/c#/Day16/Day16/Article.cs
@@ -1,22 +1,24 @@
-﻿namespace Day16;
+﻿using System.Collections.Immutable;
+
+namespace Day16;
 
 public class Article
 {
     private readonly string _content;
     private readonly string _name;
 
-    public Article(string name, string content) : this(name, content, new List<Comment>())
+    public Article(string name, string content) : this(name, content, ImmutableList<Comment>.Empty)
     {
     }
-    
+
     private Article(string name, string content, IEnumerable<Comment> comments)
     {
         _name = name;
         _content = content;
-        Comments = comments.ToList();
+        Comments = comments.ToImmutableList();
     }
 
-    public List<Comment> Comments { get; }
+    public ImmutableList<Comment> Comments { get; }
 
     private Article AddComment(
         string text,
