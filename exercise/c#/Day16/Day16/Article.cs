6844c191ea9b6259d6ca83a44108ce669a0157dc
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-02 19:21:45 +0100

    refactor: Article class constructor

--- exercise/c#/Day16/Day16/Article.cs
+++ exercise/c#/Day16/Day16/Article.cs
@@ -2,23 +2,21 @@
 
 public class Article
 {
-    private readonly string _name;
     private readonly string _content;
-    public List<Comment> Comments { get; }
+    private readonly string _name;
 
-    public Article(string name, string content)
+    public Article(string name, string content) : this(name, content, new List<Comment>())
     {
-        _name = name;
-        _content = content;
-        Comments = new List<Comment>();
     }
-
+    
     private Article(string name, string content, IEnumerable<Comment> comments)
     {
         _name = name;
         _content = content;
         Comments = comments.ToList();
     }
+
+    public List<Comment> Comments { get; }
 
     private Article AddComment(
         string text,
