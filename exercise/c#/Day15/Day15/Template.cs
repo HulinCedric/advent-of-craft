b7b135d24aa54425bb650396f8e769307cc7a0b0
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-02 09:07:30 +0100

    refactor: Template search logic by document and record type with Dictionary

--- exercise/c#/Day15/Day15/Template.cs
+++ exercise/c#/Day15/Day15/Template.cs
@@ -5,29 +5,32 @@ public record Template(
     RecordType RecordType,
     DocumentType DocumentType)
 {
-    private static IEnumerable<Template> TemplateMappings() => new[]
-    {
-        new Template(DocumentTemplate.DEERPP, RecordType.IndividualProspect, DocumentType.DEER),
-        new Template(DocumentTemplate.DEERPM, RecordType.LegalProspect, DocumentType.DEER),
-        new Template(DocumentTemplate.AUTP, RecordType.IndividualProspect, DocumentType.AUTP),
-        new Template(DocumentTemplate.AUTM, RecordType.LegalProspect, DocumentType.AUTM),
-        new Template(DocumentTemplate.SPEC, RecordType.All, DocumentType.SPEC),
-        new Template(DocumentTemplate.GLPP, RecordType.IndividualProspect, DocumentType.GLPP),
-        new Template(DocumentTemplate.GLPM, RecordType.LegalProspect, DocumentType.GLPM)
-    };
+    private static IEnumerable<Template> TemplateMappings()
+        => new[]
+        {
+            new Template(DocumentTemplate.DEERPP, RecordType.IndividualProspect, DocumentType.DEER),
+            new Template(DocumentTemplate.DEERPM, RecordType.LegalProspect, DocumentType.DEER),
+            new Template(DocumentTemplate.AUTP, RecordType.IndividualProspect, DocumentType.AUTP),
+            new Template(DocumentTemplate.AUTM, RecordType.LegalProspect, DocumentType.AUTM),
+            new Template(DocumentTemplate.SPEC, RecordType.All, DocumentType.SPEC),
+            new Template(DocumentTemplate.GLPP, RecordType.IndividualProspect, DocumentType.GLPP),
+            new Template(DocumentTemplate.GLPM, RecordType.LegalProspect, DocumentType.GLPM)
+        };
 
     public static Template FindTemplateFor(string documentType, string recordType)
     {
-            
+        var key = $"{documentType}-{recordType}";
+        var templatesByDocumentAndRecordType = TemplateMappings()
+            .ToDictionary(t => $"{t.DocumentType}-{t.RecordType}", t => t);
+        if (templatesByDocumentAndRecordType.TryGetValue(key, out var template))
+        {
+            return template;
+        }
+
         foreach (var dtt in TemplateMappings())
         {
             if (dtt.DocumentType.ToString().Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
-                dtt.RecordType.ToString().Equals(recordType, StringComparison.InvariantCultureIgnoreCase))
-            {
-                return dtt;
-            }
-            else if (dtt.DocumentType.ToString().Equals(documentType, StringComparison.InvariantCultureIgnoreCase) &&
-                     dtt.RecordType.ToString() == "All")
+                dtt.RecordType.ToString() == "All")
             {
                 return dtt;
             }
@@ -36,5 +39,6 @@ public record Template(
         throw new ArgumentException("Invalid Document template type or record type");
     }
 
-    public override string ToString() => DocumentTemplate.ToString();
+    public override string ToString()
+        => DocumentTemplate.ToString();
 }
