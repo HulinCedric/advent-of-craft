c97b3b567ce3fbcc8df03ef1928b9a5cfaecf227
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-02 18:43:36 +0100

    refactor: file-scoped namespaces

--- exercise/c#/Day16/Day16.Tests/ArticleBuilder.cs
+++ exercise/c#/Day16/Day16.Tests/ArticleBuilder.cs
@@ -1,31 +1,30 @@
-namespace Day16.Tests
+namespace Day16.Tests;
+
+public class ArticleBuilder
 {
-    public class ArticleBuilder
+    public const string Author = "Pablo Escobar";
+    public const string CommentText = "Amazing article !!!";
+    private readonly Dictionary<string, string> _comments = new();
+
+    public static ArticleBuilder AnArticle() => new();
+
+    public ArticleBuilder Commented()
     {
-        public const string Author = "Pablo Escobar";
-        public const string CommentText = "Amazing article !!!";
-        private readonly Dictionary<string, string> _comments = new();
+        _comments.Add(CommentText, Author);
+        return this;
+    }
 
-        public static ArticleBuilder AnArticle() => new();
+    public Article Build()
+    {
+        var article = new Article(
+            "Lorem Ipsum",
+            "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
+        );
 
-        public ArticleBuilder Commented()
-        {
-            _comments.Add(CommentText, Author);
-            return this;
-        }
+        _comments
+            .ToList()
+            .ForEach(comment => article.AddComment(comment.Key, comment.Value));
 
-        public Article Build()
-        {
-            var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-            );
-
-            _comments
-                .ToList()
-                .ForEach(comment => article.AddComment(comment.Key, comment.Value));
-
-            return article;
-        }
+        return article;
     }
 }
