d08cba4a6cf0d6c8869a4d9537d074fa5df704cd
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-04 22:40:01 +0100

    refactor: Move dice roll validation into Roll class

--- exercise/c#/Day20/Day20/Domain/Yahtzee/Roll.cs
+++ exercise/c#/Day20/Day20/Domain/Yahtzee/Roll.cs
@@ -2,10 +2,31 @@ namespace Day20.Domain.Yahtzee;
 
 public record Roll(int[] Dice)
 {
+    private const int RollLength = 5;
+    private const int MinimumDie = 1;
+    private const int MaximumDie = 6;
+
     public static Roll Parse(int[] dice)
     {
-        DiceRollValidator.ValidateRoll(dice);
-        
+        Validate(dice);
+
         return new Roll(dice);
     }
+
+    private static void Validate(int[] dice)
+    {
+        if (HasInvalidLength(dice))
+        {
+            throw new ArgumentException("Invalid dice... A roll should contain 5 dice.");
+        }
+
+        if (ContainsInvalidDie(dice))
+        {
+            throw new ArgumentException("Invalid die value. Each die must be between 1 and 6.");
+        }
+    }
+
+    private static bool HasInvalidLength(int[] dice) => dice is not { Length: RollLength };
+    private static bool ContainsInvalidDie(IEnumerable<int> dice) => !dice.All(IsValidDie);
+    private static bool IsValidDie(int die) => die is >= MinimumDie and <= MaximumDie;
 }
