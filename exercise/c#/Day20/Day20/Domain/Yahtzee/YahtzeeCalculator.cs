5125194abe52bea058bf0a23894f9c6e218818f1
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-04 21:58:02 +0100

    refactor: Extract DiceRollValidator

--- exercise/c#/Day20/Day20/Domain/Yahtzee/YahtzeeCalculator.cs
+++ exercise/c#/Day20/Day20/Domain/Yahtzee/YahtzeeCalculator.cs
@@ -2,10 +2,6 @@ namespace Day20.Domain.Yahtzee;
 
 public static class YahtzeeCalculator
 {
-    private const int RollLength = 5;
-    private const int MinimumDie = 1;
-    private const int MaximumDie = 6;
-
     public static int Number(int[] dice, int number)
         => Calculate(d => d.Where(die => die == number).Sum(), dice);
 
@@ -60,32 +56,9 @@ public static class YahtzeeCalculator
 
     private static int Calculate(Func<List<int>, int> compute, int[] dice)
     {
-        ValidateRoll(dice);
+        DiceRollValidator.ValidateRoll(dice);
         return compute(dice.ToList());
     }
-
-    #region Validation
-
-    private static void ValidateRoll(int[] dice)
-    {
-        if (HasInvalidLength(dice))
-        {
-            throw new ArgumentException("Invalid dice... A roll should contain 5 dice.");
-        }
-
-        if (ContainsInvalidDie(dice))
-        {
-            throw new ArgumentException("Invalid die value. Each die must be between 1 and 6.");
-        }
-    }
-
-    private static bool HasInvalidLength(int[] dice) => dice is not {Length: RollLength};
-
-    private static bool ContainsInvalidDie(IEnumerable<int> dice) => !dice.All(IsValidDie);
-
-    private static bool IsValidDie(int die) => die is >= MinimumDie and <= MaximumDie;
-
-    #endregion
 
     private static class Scores
     {
