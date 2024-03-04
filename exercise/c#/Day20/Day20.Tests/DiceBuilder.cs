85a3cffea9553f33fc81ecc29ea84ffc9f81b757
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-04 21:50:57 +0100

    refactor: File-scoped namespace

--- exercise/c#/Day20/Day20.Tests/DiceBuilder.cs
+++ exercise/c#/Day20/Day20.Tests/DiceBuilder.cs
@@ -1,16 +1,15 @@
-namespace Day20.Tests
+namespace Day20.Tests;
+
+public class DiceBuilder
 {
-    public class DiceBuilder
-    {
-        private readonly int[] _dice;
+    private readonly int[] _dice;
 
-        private DiceBuilder(int[] dice) => _dice = dice;
+    private DiceBuilder(int[] dice) => _dice = dice;
 
-        public static DiceBuilder NewRoll(int dice1, int dice2, int dice3, int dice4, int dice5)
-            => new([dice1, dice2, dice3, dice4, dice5]);
+    public static DiceBuilder NewRoll(int dice1, int dice2, int dice3, int dice4, int dice5)
+        => new([dice1, dice2, dice3, dice4, dice5]);
 
-        public int[] Build() => _dice;
+    public int[] Build() => _dice;
 
-        public override string ToString() => $"[{string.Join(", ", _dice)}]";
-    }
+    public override string ToString() => $"[{string.Join(", ", _dice)}]";
 }
