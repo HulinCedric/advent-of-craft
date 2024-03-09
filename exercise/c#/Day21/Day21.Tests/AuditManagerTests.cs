32f9577ecb9b8f0f96c19aab20b94f684d702154
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-09 14:38:14 +0100

    Third test with fake

--- exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
+++ exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
@@ -11,33 +11,25 @@ public class AuditManagerTests
     [Fact]
     public void A_New_File_Is_Created_When_The_Current_File_Overflows()
     {
-        var fileSystemMock = Substitute.For<IFileSystem>();
-        fileSystemMock.GetFiles(DirectoryName)
-            .Returns(
-                new[]
-                {
-                    Path.Combine(DirectoryName, "audit_1.txt"),
-                    Path.Combine(DirectoryName, "audit_2.txt")
-                });
+        var fakeFileSystem = new FakeFileSystem();
+        fakeFileSystem.AddFile(DirectoryName, "audit_1.txt", []);
+        fakeFileSystem.AddFile(DirectoryName, "audit_2.txt",
+        [
+            "Peter;2019-04-06 16:30:00",
+            "Jane;2019-04-06 16:40:00",
+            "Jack;2019-04-06 17:00:00"
+        ]);
 
-        fileSystemMock
-            .ReadAllLines(Path.Combine(DirectoryName, "audit_2.txt"))
-            .Returns(
-                new List<string>
-                {
-                    "Peter;2019-04-06 16:30:00",
-                    "Jane;2019-04-06 16:40:00",
-                    "Jack;2019-04-06 17:00:00"
-                });
-
-        var sut = new AuditManager(3, DirectoryName, fileSystemMock);
+        var sut = new AuditManager(3, DirectoryName, fakeFileSystem);
 
         sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
 
-        fileSystemMock.Received(1)
-            .WriteAllText(
-                Path.Combine(DirectoryName, "audit_3.txt"),
-                "Alice;2019-04-06 18:00:00");
+        fakeFileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_3.txt"))
+            .Should()
+            .BeEquivalentTo(
+            [
+                "Alice;2019-04-06 18:00:00"
+            ]);
     }
 
     [Fact]
