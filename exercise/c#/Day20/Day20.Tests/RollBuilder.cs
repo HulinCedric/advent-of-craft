a6bf4f5d1c6cdf37c87df1c7a021c8be33e3ce96
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-09 12:07:37 +0100

    Remove usage of parse unsafe

--- exercise/c#/Day20/Day20.Tests/RollBuilder.cs
+++ exercise/c#/Day20/Day20.Tests/RollBuilder.cs
@@ -6,13 +6,19 @@ public class RollBuilder
 {
     private readonly int[] _dice;
 
-    private RollBuilder(int[] dice) => _dice = dice;
+    private RollBuilder(int[] dice)
+        => _dice = dice;
 
     public static RollBuilder NewRoll(int dice1, int dice2, int dice3, int dice4, int dice5)
         => new([dice1, dice2, dice3, dice4, dice5]);
 
-    public override string ToString() => $"[{string.Join(", ", _dice)}]";
+    public override string ToString()
+        => $"[{string.Join(", ", _dice)}]";
 
     public Roll Build()
-        => Roll.ParseUnsafe(_dice);
+    {
+        Roll? roll = null;
+        Roll.Parse(_dice, roll1 => roll = roll1, failure => throw new ArgumentException(failure));
+        return roll!;
+    }
 }
