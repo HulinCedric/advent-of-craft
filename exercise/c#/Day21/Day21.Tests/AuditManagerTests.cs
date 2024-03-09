b3e7969a4afb2875c019ba0b1b38b51e98d57750
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-09 14:08:53 +0100

    test: Add missing cases

--- exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
+++ exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
@@ -38,4 +38,52 @@ public class AuditManagerTests
                 Path.Combine(DirectoryName, "audit_3.txt"),
                 "Alice;2019-04-06 18:00:00");
     }
+
+    [Fact]
+    public void Append_To_Current_File_When_Current_File_Not_Full()
+    {
+        var fileSystemMock = Substitute.For<IFileSystem>();
+        fileSystemMock.GetFiles(DirectoryName)
+            .Returns(
+                new[]
+                {
+                    Path.Combine(DirectoryName, "audit_1.txt")
+                });
+        fileSystemMock
+            .ReadAllLines(Path.Combine(DirectoryName, "audit_1.txt"))
+            .Returns(
+                new List<string>
+                {
+                    "Peter;2019-04-06 16:30:00",
+                    "Jane;2019-04-06 16:40:00"
+                });
+
+        var sut = new AuditManager(3, DirectoryName, fileSystemMock);
+
+        sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
+
+        fileSystemMock.Received(1)
+            .WriteAllText(
+                Path.Combine(DirectoryName, "audit_1.txt"),
+                "Peter;2019-04-06 16:30:00\n" +
+                "Jane;2019-04-06 16:40:00\n" +
+                "Alice;2019-04-06 18:00:00");
+    }
+
+    [Fact]
+    public void A_New_File_Is_Created_When_No_Files()
+    {
+        var fileSystemMock = Substitute.For<IFileSystem>();
+        fileSystemMock.GetFiles(DirectoryName)
+            .Returns(new string[] { });
+
+        var sut = new AuditManager(3, DirectoryName, fileSystemMock);
+
+        sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
+
+        fileSystemMock.Received(1)
+            .WriteAllText(
+                Path.Combine(DirectoryName, "audit_1.txt"),
+                "Alice;2019-04-06 18:00:00");
+    }
 }
