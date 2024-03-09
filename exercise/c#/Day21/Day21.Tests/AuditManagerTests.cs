7ade057d5b3ed3ffd1a564bc19327fe14f277f10
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-09 14:40:17 +0100

    Simplify setup

--- exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
+++ exercise/c#/Day21/Day21.Tests/AuditManagerTests.cs
@@ -6,24 +6,26 @@ namespace Day21.Tests;
 public class AuditManagerTests
 {
     private const string DirectoryName = "audits";
+    private readonly FakeFileSystem _fileSystem = new();
 
     [Fact]
     public void A_New_File_Is_Created_When_The_Current_File_Overflows()
     {
-        var fakeFileSystem = new FakeFileSystem();
-        fakeFileSystem.AddFile(DirectoryName, "audit_1.txt", []);
-        fakeFileSystem.AddFile(DirectoryName, "audit_2.txt",
-        [
-            "Peter;2019-04-06 16:30:00",
-            "Jane;2019-04-06 16:40:00",
-            "Jack;2019-04-06 17:00:00"
-        ]);
+        _fileSystem.AddFile(DirectoryName, "audit_1.txt", []);
+        _fileSystem.AddFile(
+            DirectoryName,
+            "audit_2.txt",
+            [
+                "Peter;2019-04-06 16:30:00",
+                "Jane;2019-04-06 16:40:00",
+                "Jack;2019-04-06 17:00:00"
+            ]);
 
-        var sut = new AuditManager(3, DirectoryName, fakeFileSystem);
+        var sut = new AuditManager(3, DirectoryName, _fileSystem);
 
         sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
 
-        fakeFileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_3.txt"))
+        _fileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_3.txt"))
             .Should()
             .BeEquivalentTo(
             [
@@ -34,18 +36,19 @@ public class AuditManagerTests
     [Fact]
     public void Append_To_Current_File_When_Current_File_Not_Full()
     {
-        var fakeFileSystem = new FakeFileSystem();
-        fakeFileSystem.AddFile(DirectoryName, "audit_1.txt",
-        [
-            "Peter;2019-04-06 16:30:00",
-            "Jane;2019-04-06 16:40:00"
-        ]);
-        
-        var sut = new AuditManager(3, DirectoryName, fakeFileSystem);
+        _fileSystem.AddFile(
+            DirectoryName,
+            "audit_1.txt",
+            [
+                "Peter;2019-04-06 16:30:00",
+                "Jane;2019-04-06 16:40:00"
+            ]);
+
+        var sut = new AuditManager(3, DirectoryName, _fileSystem);
 
         sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
 
-        fakeFileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_1.txt"))
+        _fileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_1.txt"))
             .Should()
             .BeEquivalentTo(
             [
@@ -58,13 +61,11 @@ public class AuditManagerTests
     [Fact]
     public void A_New_File_Is_Created_When_No_Files()
     {
-        var fakeFileSystem = new FakeFileSystem();
-
-        var sut = new AuditManager(3, DirectoryName, fakeFileSystem);
+        var sut = new AuditManager(3, DirectoryName, _fileSystem);
 
         sut.AddRecord("Alice", DateTime.Parse("2019-04-06T18:00:00"));
 
-        fakeFileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_1.txt"))
+        _fileSystem.ReadAllLines(Path.Combine(DirectoryName, "audit_1.txt"))
             .Should()
             .BeEquivalentTo(
             [
