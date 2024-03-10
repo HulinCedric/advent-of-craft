6e3fe56af839fb319b62d170e76400929a2a2618
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-10 10:10:11 +0100

    feat: Transpose logic

--- exercise/c#/Day21/Day21/FunctionalCore_ImperativeShell/AuditManager.cs
+++ exercise/c#/Day21/Day21/FunctionalCore_ImperativeShell/AuditManager.cs
@@ -2,6 +2,22 @@ namespace Day21.FunctionalCore_ImperativeShell;
 
 public class AuditManager(int maxEntriesPerFile)
 {
-    public FileUpdated AddRecord(List<FileContent> files, string visitorName, DateTime timeOfVisit) =>
-        new("audit_3.txt", "Alice;2019-04-06 18:00:00");
+    public FileUpdated AddRecord(List<FileContent> files, string visitorName, DateTime timeOfVisit)
+    {
+        var sorted = SortByIndex(files);
+        var newRecord = visitorName + ';' + timeOfVisit.ToString("yyyy-MM-dd HH:mm:ss");
+
+        var currentFile = sorted.Last();
+        var currentFileIndex = sorted.Count;
+
+        var newIndex = currentFileIndex + 1;
+        var newName = $"audit_{newIndex}.txt";
+        
+        return new FileUpdated(newName, newRecord);
+    }
+    
+    private static List<FileContent> SortByIndex(List<FileContent> files) =>
+        files
+            .OrderBy(x => x.FileName)
+            .ToList();
 }
