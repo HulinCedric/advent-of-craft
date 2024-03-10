35d6191918efb935cbc137e0388b6d97b47965ad
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-10 10:30:56 +0100

    refactor: Simplify AddRecord method

--- exercise/c#/Day21/Day21/FunctionalCore_ImperativeShell/AuditManager.cs
+++ exercise/c#/Day21/Day21/FunctionalCore_ImperativeShell/AuditManager.cs
@@ -5,29 +5,34 @@ public class AuditManager(int maxEntriesPerFile)
     public FileUpdated AddRecord(List<FileContent> files, string visitorName, DateTime timeOfVisit)
     {
         var sorted = SortByIndex(files);
-        var newRecord = visitorName + ';' + timeOfVisit.ToString("yyyy-MM-dd HH:mm:ss");
+        var newRecord = CreateNewRecord(visitorName, timeOfVisit);
 
-        if (sorted.Count == 0)
-        {
-            return new FileUpdated("audit_1.txt", newRecord);
-        }
-
-        var currentFile = sorted.Last();
-        if (currentFile.Lines.Count < maxEntriesPerFile)
-        {
-            var newContent = currentFile.Lines.Append(newRecord);
-            return new FileUpdated(currentFile.FileName, string.Join(Environment.NewLine, newContent));
-        }
-
-        var currentFileIndex = sorted.Count;
-        var newIndex = currentFileIndex + 1;
-        var newName = $"audit_{newIndex}.txt";
-
-        return new FileUpdated(newName, newRecord);
+        return sorted.Count == 0
+                   ? new FileUpdated("audit_1.txt", newRecord)
+                   : CreateNewFileOrUpdate(sorted, newRecord);
     }
 
     private static List<FileContent> SortByIndex(List<FileContent> files) =>
         files
             .OrderBy(x => x.FileName)
             .ToList();
+
+    private static string CreateNewRecord(string visitorName, DateTime timeOfVisit) =>
+        visitorName + ';' + timeOfVisit.ToString("yyyy-MM-dd HH:mm:ss");
+
+    private FileUpdated CreateNewFileOrUpdate(List<FileContent> sortedFiles, string newRecord)
+    {
+        var currentFileIndex = sortedFiles.Count;
+        var newIndex = currentFileIndex + 1;
+        var newName = $"audit_{newIndex}.txt";
+
+        var currentFile = sortedFiles.Last();
+        if (currentFile.Lines.Count < maxEntriesPerFile)
+        {
+            var newContent = currentFile.Lines.Append(newRecord);
+            return new FileUpdated(currentFile.FileName, string.Join(Environment.NewLine, newContent));
+        }
+
+        return new FileUpdated(newName, newRecord);
+    }
 }
