81ccabdb38832d3d3386b977c4a9d3cd6a7aed04
Cédric Hulin <hulin.cedric@gmail.com>
2024-03-24 22:09:41 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 0
        test: 15
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- exercise/c#/Day22/Day22.Tests/DiamondShould.cs
+++ exercise/c#/Day22/Day22.Tests/DiamondShould.cs
@@ -1,7 +1,22 @@
+using FsCheck.Xunit;
+
 namespace Day22.Tests;
 
 public class DiamondShould
 {
-   
+    [Property]
+    public bool Not_be_empty(char letter)
+    {
+        var diamond = Diamond.Print(letter);
+        return string.IsNullOrWhiteSpace(diamond) == false;
+    }
+}
+
+public class Diamond
+{
+    public static string Print(char letter)
+    {
+        return "toto";
+    }
 }
 
